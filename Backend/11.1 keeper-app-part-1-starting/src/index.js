76be3310611000e7b6c55941a93ad98ec5d5daa7
Anusree <anilkumaranusree113@gmail.com>
2023-10-09 22:53:08 +0530

    upto keeper app 1

--- Backend/11.1 keeper-app-part-1-starting/src/index.js
+++ Backend/11.1 keeper-app-part-1-starting/src/index.js
@@ -11,3 +11,8 @@
 
 //HINT: You will need to study the classes in the styles.css file to appy styling.
 
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
