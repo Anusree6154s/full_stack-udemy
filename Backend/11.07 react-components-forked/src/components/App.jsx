34a6c447b4650d2acd3ca4cc21acb59e12b7f5f8
Anusree <anilkumaranusree113@gmail.com>
2023-10-11 02:49:05 +0530

    react

---
+++ Backend/11.07 react-components-forked/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import List from "./List";
+import Heading from "./Heading";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ Backend/11.09 es6-import-export-and-modules-forked/src/math.js
@@ -0,0 +1,12 @@
+const pi = 3.14159265358;
+
+function doublePi() {
+  return pi * pi;
+}
+
+function triplePi() {
+  return pi * pi * pi;
+}
+
+export default pi;
+export { doublePi, triplePi };
